a4b6fa9f564ae8f9457fcd52d85632dc207663f0
Yugang Cao <1442770751@qq.com>
2023-11-19 10:55:52 +0800

    fix: debug (#11)

--- .github/actions/code-stats.js
+++ .github/actions/code-stats.js
@@ -119,6 +119,7 @@ function updateReadme(dailyCodeChanges) {
 async function main() {
 
   // const user = await getUsername()
+  const user = process.env.GITHUB_ACTOR;
   const START_DATE =  moment().subtract(1, 'days').format('YYYY-MM-DD');
   const END_DATE = moment().subtract(1, 'days').format('YYYY-MM-DD');
   console.log('START_DATE', START_DATE)
